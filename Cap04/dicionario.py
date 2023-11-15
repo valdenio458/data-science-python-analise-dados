4b6770ffffef37707a5e7228a0289ddc79a34e06
valdenio458 <valdeniomartinho@gmail.com>
2023-11-15 16:47:22 -0300

    capítulo 04

---
+++ Cap04/dicionario.py
@@ -0,0 +1,9 @@
+estudantes = {"Pedro": 24, "Ana": 22, "Ronaldo": 26, "Janaína": 25}
+
+print(estudantes.keys())
+print(estudantes.values())
+print(estudantes.items())
+
+carnes = {"ch1": 'picanha', "ch2": 'alcatra'}
+print(carnes["ch1"].upper())  # PICANHA
+print(carnes)  # {'ch1': 'picanha', 'ch2': 'alcatra'}
